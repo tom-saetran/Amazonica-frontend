39074480079955e1338dde95ab1522dc1eb0d05d
Tom-Lennart Sætran <79971807+tom-saetran@users.noreply.github.com>
2021-05-21 12:03:25 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,11 +8,216 @@ import Footer from "./views/Home"
 import React from "react"
 
 class App extends React.Component {
+    state = {
+        endpoint: ""
+    }
     crud = {
-        get: {},
-        post: {},
-        put: {},
-        delete: {}
+        products: {
+            get: async id => {
+                let results
+                try {
+                    if (id === "" || id === undefined || id === null) throw new Error("id must be present")
+                    results = await fetch(this.state.endpoint + "products/" + id)
+                    if (!results.ok) throw new Error("got data in return but the ok flag is not true! response: " + results)
+                    results = await results.json()
+                } catch (error) {
+                    console.error(error)
+                    return null
+                }
+                return await results
+            },
+
+            getAll: async () => {
+                let results
+                try {
+                    results = await fetch(this.state.endpoint + "products/")
+                    if (!results.ok) throw new Error("got data in return but the ok flag is not true! response: " + results)
+                    results = await results.json()
+                } catch (error) {
+                    console.error(error)
+                    return null
+                }
+                return await results
+            },
+
+            getReviews: async id => {
+                let results
+                try {
+                    if (id === "" || id === undefined || id === null) throw new Error("id must be present")
+                    results = await fetch(this.state.endpoint + "products/" + id + "/reviews")
+                    if (!results.ok) throw new Error("got data in return but the ok flag is not true! response: " + results)
+                    results = await results.json()
+                } catch (error) {
+                    console.error(error)
+                    return null
+                }
+                return await results
+            },
+
+            post: async data => {
+                let results
+                try {
+                    if (typeof data !== "object") throw new Error("data must be an object")
+                    results = await fetch(this.state.endpoint + "products/", {
+                        method: "POST",
+                        headers: {
+                            "Content-Type": "application/json"
+                        },
+                        body: JSON.stringify(data)
+                    })
+                    if (!results.ok) throw new Error("got data in return but the ok flag is not true! response: " + results)
+                    if (results.status !== 201) return false
+                    results = await results.json()
+                } catch (error) {
+                    console.error(error)
+                    return null
+                }
+                return await results
+            },
+
+            postImage: async (id, data) => {
+                let results
+                try {
+                    if (id === "" || id === undefined || id === null) throw new Error("id must be present")
+                    if (data === "" || data === undefined || data === null) throw new Error("data must be present")
+                    results = await fetch(this.state.endpoint + "products/" + id + "/upload", {
+                        method: "POST",
+                        body: data
+                    })
+                    if (!results.ok) throw new Error("got data in return but the ok flag is not true! response: " + results)
+                    if (results.status !== 201) return false
+                    results = await results.json()
+                } catch (error) {
+                    console.error(error)
+                    return null
+                }
+                return await results
+            },
+
+            put: async (id, data) => {
+                let results
+                try {
+                    if (id === "" || id === undefined || id === null) throw new Error("id must be present")
+                    if (typeof data !== "object") throw new Error("data must be an object")
+                    results = await fetch(this.state.endpoint + "products/" + id, {
+                        method: "PUT",
+                        headers: {
+                            "Content-Type": "application/json"
+                        },
+                        body: JSON.stringify(data)
+                    })
+                    if (!results.ok) throw new Error("got data in return but the ok flag is not true! response: " + results)
+                    results = await results.json()
+                } catch (error) {
+                    console.error(error)
+                    return null
+                }
+                return await results
+            },
+
+            delete: async id => {
+                let results
+                try {
+                    if (id === "" || id === undefined || id === null) throw new Error("id must be present")
+                    results = await fetch(this.state.endpoint + "products/" + id, {
+                        method: "DELETE"
+                    })
+                    if (!results.ok) throw new Error("got data in return but the ok flag is not true! response: " + results)
+                    results = await results.json()
+                } catch (error) {
+                    console.error(error)
+                    return null
+                }
+                return await results
+            }
+        },
+
+        reviews: {
+            get: async id => {
+                let results
+                try {
+                    if (id === "" || id === undefined || id === null) throw new Error("id must be present")
+                    results = await fetch(this.state.endpoint + "reviews/" + id)
+                    if (!results.ok) throw new Error("got data in return but the ok flag is not true! response: " + results)
+                    results = await results.json()
+                } catch (error) {
+                    console.error(error)
+                    return null
+                }
+                return await results
+            },
+
+            getAll: async () => {
+                let results
+                try {
+                    results = await fetch(this.state.endpoint + "reviews/")
+                    if (!results.ok) throw new Error("got data in return but the ok flag is not true! response: " + results)
+                    results = await results.json()
+                } catch (error) {
+                    console.error(error)
+                    return null
+                }
+                return await results
+            },
+
+            post: async data => {
+                let results
+                try {
+                    if (typeof data !== "object") throw new Error("data must be an object")
+                    results = await fetch(this.state.endpoint + "reviews/", {
+                        method: "POST",
+                        headers: {
+                            "Content-Type": "application/json"
+                        },
+                        body: JSON.stringify(data)
+                    })
+                    if (!results.ok) throw new Error("got data in return but the ok flag is not true! response: " + results)
+                    if (results.status !== 201) return false
+                    results = await results.json()
+                } catch (error) {
+                    console.error(error)
+                    return null
+                }
+                return await results
+            },
+
+            put: async (id, data) => {
+                let results
+                try {
+                    if (id === "" || id === undefined || id === null) throw new Error("id must be present")
+                    if (typeof data !== "object") throw new Error("data must be an object")
+                    results = await fetch(this.state.endpoint + "reviews/" + id, {
+                        method: "PUT",
+                        headers: {
+                            "Content-Type": "application/json"
+                        },
+                        body: JSON.stringify(data)
+                    })
+                    if (!results.ok) throw new Error("got data in return but the ok flag is not true! response: " + results)
+                    results = await results.json()
+                } catch (error) {
+                    console.error(error)
+                    return null
+                }
+                return await results
+            },
+
+            delete: async id => {
+                let results
+                try {
+                    if (id === "" || id === undefined || id === null) throw new Error("id must be present")
+                    results = await fetch(this.state.endpoint + "reviews/" + id, {
+                        method: "DELETE"
+                    })
+                    if (!results.ok) throw new Error("got data in return but the ok flag is not true! response: " + results)
+                    results = await results.json()
+                } catch (error) {
+                    console.error(error)
+                    return null
+                }
+                return await results
+            }
+        }
     }
 
     render() {
